0cfda2f734f9de8517fc5adff2486392bf5ed8bb
Raul <raul@raul.com>
2018-11-13 17:14:26 -0400

    update (partial)

--- src/Update.elm
+++ src/Update.elm
@@ -6,4 +6,13 @@ import Msgs exposing (Msg(..))
 
 update : Msg -> Model -> Model
 update msg model =
-    
+    case msg of 
+        Input name -> {model | name = name}
+
+        Cancel -> {model | name = "", playerId = Nothing}
+
+        Save -> savePlayer model
+
+savePlayer: model =
+    if model.name == "" then
+        
